8ec8a85c54ab35e99c25f2149c64cbd84459138c
Ricardo Medina <r.medina2119@gmail.com>
2025-02-03 16:38:54 -0800

    updated padding on header so that its not so close to the top of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,9 +83,9 @@ const ChatPage = () => {
   };
   return (
     <div className="flex flex-col h-screen pb-10 bg-white">
-      <div className="w-full flex items-center px-4 py-4 justify-center ">
+      <div className="w-full flex items-center px-4 py-6 justify-center ">
         <header>
-          <h1 className="text-[2rem] font-extrabold text-black">ASK BOT</h1>
+          <h1 className="text-[3rem] font-extrabold text-black">ASK BOT</h1>
         </header>
       </div>
 
